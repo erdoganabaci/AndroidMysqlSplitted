3943a501d5ab6d6013b1545daf47bc51b8a4266a
unknown <erdoganabaci97@gmail.com>
2019-07-10 15:35:36 +0300

    push notification eklendi

--- app/src/main/java/mysqltest/app/mysqlconnnection2/MainActivity.java
+++ app/src/main/java/mysqltest/app/mysqlconnnection2/MainActivity.java
@@ -2,11 +2,22 @@ package mysqltest.app.mysqlconnnection2;
 
 import android.os.AsyncTask;
 import android.os.Handler;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.TextView;
 import android.widget.Toast;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+import com.onesignal.OneSignal;
+
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import java.io.PrintWriter;
 import java.io.StringWriter;
@@ -16,7 +27,10 @@ import java.sql.ResultSet;
 import java.sql.Statement;
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 import java.util.Calendar;
+import java.util.HashMap;
+import java.util.UUID;
 
 public class MainActivity extends AppCompatActivity {
     TextView textView ;
@@ -25,11 +39,25 @@ public class MainActivity extends AppCompatActivity {
     public static final String DB_URL1 = "jdbc:mysql://localhost:8080/erdogan?user=erdogan&password=123456&useUnicode=true&characterEncoding=UTF-8";
     private static final String USER = "erdogan";
     private static final String PASS = "123456";
-
+    DatabaseReference mDatabase;
+    Boolean compareString2;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        UUID uuid = UUID.randomUUID();
+        String uuidString = uuid.toString();
+
+        //mDatabase = FirebaseDatabase.getInstance().getReference();
+        //mDatabase.child("users").child(uuidString).setValue("ddd");
+
+
+        // OneSignal Initialization
+
+        OneSignal.startInit(this)
+                .inFocusDisplaying(OneSignal.OSInFocusDisplayOption.Notification)
+                .unsubscribeWhenNotificationsAreDisabled(true)
+                .init();
 
         final Handler handler = new Handler();
         textView = findViewById(R.id.textView);
@@ -43,17 +71,63 @@ public class MainActivity extends AppCompatActivity {
                 try{
                     Send objSend = new Send();
                     objSend.execute();
+
+                    mDatabase = FirebaseDatabase.getInstance().getReference();
+                    OneSignal.idsAvailable(new OneSignal.IdsAvailableHandler() {
+                        @Override
+                        public void idsAvailable(final String userId, String registrationId) {
+                            System.out.println("userid: "+userId);
+                            UUID uuid = UUID.randomUUID();
+                            final String uuidString = uuid.toString();
+                            //her seferinde eklencek.aynı player istemediğimiz durum.
+                            DatabaseReference newReference = FirebaseDatabase.getInstance().getReference("PlayerIds");
+                            newReference.addValueEventListener(new ValueEventListener() {
+                                @Override
+                                public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                                    ArrayList<String> playerIDsFromServer = new ArrayList<>();
+                                    for (DataSnapshot ds : dataSnapshot.getChildren())
+                                    {   //gelen data hasmap tarzında
+                                        HashMap <String,String> hashMap = (HashMap<String, String>) ds.getValue();
+                                        String currentPlayerID = hashMap.get("playerID");
+                                        playerIDsFromServer.add(currentPlayerID);
+                                        if (compareString2 == true){
+                                            try {
+                                                OneSignal.postNotification(new JSONObject("{'contents': {'en':'İçerik Güncellendi'}, 'include_player_ids': ['" + currentPlayerID + "']}"), null);
+                                            } catch (JSONException e) {
+                                                e.printStackTrace();
+                                            }
+                                        }
+                                    }
+                                    if (!playerIDsFromServer.contains(userId)){
+                                        mDatabase.child("PlayerIds").child(uuidString).child("playerID").setValue(userId);
+
+                                    }
+                                }
+
+                                @Override
+                                public void onCancelled(@NonNull DatabaseError databaseError) {
+
+                                }
+                            });
+
+                        }
+                    });
                 }
                 catch (Exception e) {
 
                 }
                 finally{
                     //also call the same runnable to call it at regular interval
-                    handler.postDelayed(this, 10000);
+                    handler.postDelayed(this, 2000);
                 }
             }
         };
-        handler.postDelayed(runnable, 10000);
+        handler.postDelayed(runnable, 2000);
+
+
+
+
+
 
 
 
@@ -145,8 +219,8 @@ public class MainActivity extends AppCompatActivity {
             //Toast.makeText(getApplicationContext(),clockCombineTime,Toast.LENGTH_SHORT).show();
             Boolean compareString = dateMsgIndex.equals(dateCurrIndex);
             //Toast.makeText(getApplicationContext(),compareString.toString(),Toast.LENGTH_LONG).show();
-            String myString = "hello erdocum";
-            Boolean compareString2 = msg.equals(myString);
+            String myString = "merhaba erdo";
+            compareString2 = msg.equals(myString);
             Toast.makeText(getApplicationContext(),compareString2.toString(),Toast.LENGTH_LONG).show();
             textView2.setText(date);
         }
